78beb69024688da61ea5d6a94d311e4784ab3a99
Felipe Madson <felipemadson190@email.com>
2025-12-15 22:28:26 -0300

    feat: Feat: Configuração das rotas (URLs) para Carros e Aluguéis na API

--- cconfig/urls.py
+++ cconfig/urls.py
@@ -4,13 +4,22 @@ from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken.views import obtain_auth_token
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
-# Importando as views que criamos
+# Importando as views que criamos (Sua parte - Diego)
 from accounts.views import UserViewSet, PerfilClienteViewSet
+
+# Importando as views do Felipe (NOVO - App Core)
+from core.views import CarroViewSet, AluguelViewSet
 
 # Configurando o roteador (gera as rotas automaticamente)
 router = DefaultRouter()
+
+# Registros da parte do Diego
 router.register(r'users', UserViewSet)
 router.register(r'perfis-clientes', PerfilClienteViewSet)
+
+# Registros da parte do Felipe (NOVO)
+router.register(r'carros', CarroViewSet)
+router.register(r'alugueis', AluguelViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
